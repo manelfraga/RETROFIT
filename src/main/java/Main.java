e2412a456bbcf6f20b11c9d28130fed61fc32a78
irega97 <ivanreq97@gmail.com>
2020-04-09 00:09:47 +0200

    Login Interceptor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import okhttp3.*;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,9 +11,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Repositories");
 
+        //Creamos interceptor
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        //Creamos cliente
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
+        //Crear retrofit
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         GitHubService service = retrofit.create(GitHubService.class);
